77e1beac30f2b326ce854a1078cf40a87865e94d
Antonio Georgiev <antoniogeorgiev@Antonios-MacBook-Air.local>
2022-12-17 22:32:44 +0200

    Initialized the react typescript boilerplate

---
+++ src/components/layouts/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div>
+      <h1>React Typescript Boilerplate</h1>
+    </div>
+  );
+}
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ProgressWebpackPlugin = require("progress-webpack-plugin");
+module.exports = {
+  entry: "./src/index.tsx",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    ProgressWebpackPlugin(true),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.scss?$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          "@teamsupercell/typings-for-css-modules-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                exportLocalsConvention: "camelCase",
+                localIdentName: "[name]_[local]",
+              },
+              sourceMap: true,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};

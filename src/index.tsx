07c7666415e5eed50af8becd8ad80933a86c147f
tonigeorgiev <antonio.g.georgiev1998@gmail.com>
2022-12-19 00:49:59 +0200

    Changed the importing in the index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+import App from "@src/components/layouts/App";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/layouts/App";
 import "./styles/global.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
